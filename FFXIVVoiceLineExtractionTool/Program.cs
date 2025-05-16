cb225d0b9da50ba9771e4642d9567243964bab6f
argentum-frog <211974319+argentum-frog@users.noreply.github.com>
2025-05-16 19:39:52 +0200

    Add cutscene voice line extraction functionality

--- FFXIVVoiceLineExtractionTool/Program.cs
+++ FFXIVVoiceLineExtractionTool/Program.cs
@@ -10,7 +10,7 @@ internal class Program
 		public string OutDirectory;
 		public List<string> Languages;
 
-		public Range ExpansionRange;
+		public Tuple<uint, uint> ExpansionRange;
 	}
 
 	// On Windows typically something like:
@@ -29,8 +29,8 @@ internal class Program
 		List<string> languages = [];
 		bool extractBattleVoiceLines = false;
 		bool extractCutsceneVoiceLines = false;
-		int expansionRangeStart = 0;
-		int expansionRangeEnd = 0;
+		uint expansionRangeStart = uint.MaxValue;
+		uint expansionRangeEnd = uint.MaxValue;
 		for (UInt32 i = 0; i < args.Length; i++)
 		{
 			string arg = args[i];
@@ -68,12 +68,12 @@ internal class Program
 						if (exSelectionArg.Contains('-'))
 						{
 							string[] exSelectionRange = exSelectionArg.Split('-');
-							expansionRangeStart = int.Parse(exSelectionRange[0]);
-							expansionRangeEnd = int.Parse(exSelectionRange[1]) + 1;
+							expansionRangeStart = uint.Parse(exSelectionRange[0]);
+							expansionRangeEnd = uint.Parse(exSelectionRange[1]) + 1;
 						}
 						else
 						{
-							expansionRangeStart = int.Parse(exSelectionArg);
+							expansionRangeStart = uint.Parse(exSelectionArg);
 							expansionRangeEnd = expansionRangeStart + 1;
 						}
 					}
@@ -105,7 +105,7 @@ internal class Program
 			{
 				OutDirectory = outDirectory,
 				Languages = languages,
-				ExpansionRange = new(new(expansionRangeStart), new(expansionRangeEnd))
+				ExpansionRange = new(expansionRangeStart, expansionRangeEnd)
 			};
 			if (extractBattleVoiceLines)
 			{
@@ -125,13 +125,16 @@ internal class Program
 		System.IO.Directory.CreateDirectory(outDirectory);
 
 		const ulong start = 8201000;
-		using var logFileStream = System.IO.File.Create(outDirectory + DateTime.UtcNow.ToString("s", System.Globalization.CultureInfo.InvariantCulture).Replace(":", "-") + ".log");
+		using var logFileStream = System.IO.File.Create(outDirectory
+			+ DateTime.UtcNow.ToString("s", System.Globalization.CultureInfo.InvariantCulture).Replace(":", "-")
+			+ "_battle.log"
+		);
 		logFileStream.Write(System.Text.Encoding.UTF8.GetBytes(
 			VoLineGameDirectory + ", " + "sha256\n"
 			));
-		for (ulong i = start; i < start + (1u << 16); i++)
+		for (ulong i = start; i < start + (1u << 16); i++) // TODO change this to break at long streaks of nothing
 		{
-			string fileName = i + "_jp.scd"; // use jp language file as "canary"
+			string fileName = i + "_ja.scd"; // use JP language file as "canary"
 			if (gameData.FileExists(VoLineGameDirectory + fileName))
 			{
 				foreach (string language in extractionConfiguration.Languages)
@@ -141,7 +144,7 @@ internal class Program
 					file!.SaveFile(outDirectory + fileName); // TODO test what the difference is to SaveFileRaw
 
 					logFileStream.Write(System.Text.Encoding.UTF8.GetBytes(
-						fileName + ", " + file!.GetFileHash()
+						fileName + ", " + file!.GetFileHash() + "\n"
 					));
 				}
 			}
@@ -150,6 +153,88 @@ internal class Program
 
 	static void ExtractCutsceneVoiceLines(Lumina.GameData gameData, ExtractionConfiguration extractionConfiguration)
 	{
-		throw new NotImplementedException("Extracting cutscene voice lines is not yet implemented!");
+		string outDirectory = extractionConfiguration.OutDirectory;
+		var expansionRange = extractionConfiguration.ExpansionRange;
+
+		System.IO.Directory.CreateDirectory(outDirectory);
+		using var logFileStream = System.IO.File.Create(
+			outDirectory
+			+ DateTime.UtcNow.ToString("s", System.Globalization.CultureInfo.InvariantCulture).Replace(":", "-")
+			+ "_cutscene.log"
+		);
+		for (uint ex = expansionRange.Item1; ex < expansionRange.Item2; ex++)
+		{
+			uint num_in_ex = 0;
+			uint num_empty_patch_suffixes = 0;
+			uint last_patch_suffix = uint.MaxValue;
+			for (uint patch_suffix = 0; patch_suffix < 1000u; patch_suffix++)
+			{
+				uint num_in_patch_suffix = 0;
+				string cutsceneVoLineGameDirectory = (ex == 0 && patch_suffix < 10) ?
+					$"cut/ffxiv/sound/manfst/manfst{patch_suffix:D1}00/" // 2.0 jank
+					: $"cut/ex{ex}/sound/voicem/voiceman_{ex + 2:D2}{patch_suffix:D3}/".Replace("ex0", "ffxiv");
+				// Note that 2.1 doesn't seem to have any voiced cutscenes so only `patch_suffix` >= 200 is actually used
+
+				uint num_empty_banks = 0;
+				for (uint bank = 0; bank < (26 + 10); bank++)
+				{
+					// seems to be only used up to HW (not 100% sure though), with later Expansions simply having everything in bank 0
+					char bankChar = (char)(bank + 0x30) > '9' ? (char)(bank + 0x57) : (char)(bank + 0x30); // '0'-'9''a'-'z'
+					uint num_in_bank = 0;
+
+					uint num_unused_indices = 0;
+					for (uint i = 0; i < 100000u; i++)
+					{
+						string fileName = (ex == 0 && patch_suffix < 10) ?
+							$"vo_manfst{patch_suffix:D1}00_{bankChar}{i:D5}_m_ja.scd" // 2.0 jank
+							: $"vo_voiceman_{ex + 2:D2}{patch_suffix:D3}_{bankChar}{i:D5}_m_ja.scd"; // use JP language file as "canary"
+						if (gameData.FileExists(cutsceneVoLineGameDirectory + fileName))
+						{
+							num_in_ex += 1;
+							num_in_patch_suffix += 1;
+							num_in_bank += 1;
+							num_unused_indices = 0;
+							if (patch_suffix != last_patch_suffix)
+							{
+								System.IO.Directory.CreateDirectory(outDirectory + cutsceneVoLineGameDirectory);
+								logFileStream.Write(System.Text.Encoding.UTF8.GetBytes(
+									cutsceneVoLineGameDirectory + ", " + "sha256\n"
+									));
+								last_patch_suffix = patch_suffix;
+							}
+							foreach (string language in extractionConfiguration.Languages)
+							{
+								fileName = (ex == 0 && patch_suffix < 10) ?
+									$"vo_manfst{patch_suffix:D1}00_{bankChar}{i:D5}_m_{language}.scd" // 2.0 jank
+									: $"vo_voiceman_{ex + 2:D2}{patch_suffix:D3}_{bankChar}{i:D5}_m_{language}.scd";
+								Lumina.Data.FileResource file = gameData.GetFile(cutsceneVoLineGameDirectory + fileName)!;
+								file!.SaveFile(outDirectory + fileName); // TODO test what the difference is to SaveFileRaw
+
+								logFileStream.Write(System.Text.Encoding.UTF8.GetBytes(
+									fileName + ", " + file!.GetFileHash() + "\n"
+								));
+							}
+						}
+						else
+						{
+							num_unused_indices += 1;
+							if (num_unused_indices >= 10) break; // heuristic
+						}
+					}
+
+					if (num_in_bank == 0) num_empty_banks += 1;
+					else num_empty_banks = 0;
+					if (num_empty_banks >= (ex == 0 ? 8u : 1u)) break; // heuristic
+																	   // cut/ffxiv/sound/voicem/voiceman_02401 starts with bank 3 for some reason and others for 2.X contain jumps as well
+				}
+
+				if (num_in_patch_suffix == 0) num_empty_patch_suffixes += 1;
+				else num_empty_patch_suffixes = 0;
+				if (num_empty_patch_suffixes >= (ex == 0 ? 200u : 100u)) break; // heuristic
+			}
+
+			if (num_in_ex == 0) break;
+		}
+
 	}
 }
